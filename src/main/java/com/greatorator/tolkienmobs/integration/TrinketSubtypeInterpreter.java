761f86c0582bf866d79854aaa752c2bbb85390d5
GreatOrator <lord.technodragon@gmail.com>
2025-04-13 15:54:32 -0700

    Boats re-added

--- src/main/java/com/greatorator/tolkienmobs/integration/TrinketSubtypeInterpreter.java
+++ src/main/java/com/greatorator/tolkienmobs/integration/TrinketSubtypeInterpreter.java
@@ -1,24 +1,42 @@
 package com.greatorator.tolkienmobs.integration;
 
+import com.greatorator.tolkienmobs.handler.data.TrinketComponent;
 import com.greatorator.tolkienmobs.init.TolkienDataComponents;
 import mezz.jei.api.ingredients.subtypes.ISubtypeInterpreter;
 import mezz.jei.api.ingredients.subtypes.UidContext;
+import net.minecraft.core.Holder;
+import net.minecraft.core.component.DataComponentType;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.alchemy.Potion;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class TrinketSubtypeInterpreter implements ISubtypeInterpreter<ItemStack> {
+	public String apply(ItemStack stack, UidContext context) {
+		TrinketComponent potionContents = stack.getOrDefault(TolkienDataComponents.POTION_CONTENTS, TrinketComponent.EMPTY);
+		Optional<Holder<Potion>> potionType = potionContents.potion();
+
+		if (context != UidContext.Recipe) {
+			if (!potionContents.hasEffects()) {
+				return "";
+			}
+			MobEffectInstance mobEffectInstance = potionType.get().value().getEffects().getFirst();
+			return mobEffectInstance.getEffect().getKey().location() + "@" + mobEffectInstance.getAmplifier() + "@" + mobEffectInstance.getDuration();
+		}
+		return "";
+	}
+
 	@Override
 	public @Nullable Object getSubtypeData(ItemStack ingredient, UidContext context) {
-		return List.of(
-				ingredient.getOrDefault(TolkienDataComponents.POTION_CONTENTS, 0L)
-		);
+		String data = apply(ingredient, context);
+		return data.isEmpty() ? null : data;
 	}
 
 	@Override
 	public String getLegacyStringSubtypeInfo(ItemStack ingredient, UidContext context) {
-		return null;
+		return apply(ingredient, context);
 	}
 }
